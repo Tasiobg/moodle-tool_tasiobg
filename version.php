0a00088573fca764d3822f7194362308e9619caf
Tasio <tasiobg@gmail.com>
2025-03-03 10:28:23 +0100

    remove comment

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2025021015;
 $plugin->requires  = 2024100701.09;
 $plugin->component = 'tool_tasiobg'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_STABLE; // The plugin is a pre-release version.
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.1';
